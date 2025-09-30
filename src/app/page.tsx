fafae7346e8cd0ef9d0b91553be85e6f382d2b61
Obuleswar <obuleswaror@gmail.com>
2025-09-30 01:32:06 +0000

    Remove all code just show blank page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,3 @@
-import { AppShowcase } from "@/components/AppShowcase";
-import { getApps } from "@/lib/data";
-
-export default function Home({
-  searchParams,
-}: {
-  searchParams?: {
-    search?: string;
-  };
-}) {
-  const apps = getApps();
-  const searchQuery = '';
-
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <AppShowcase apps={apps} searchQuery={searchQuery} />
-    </div>
-  );
+export default function Home() {
+  return null;
 }
