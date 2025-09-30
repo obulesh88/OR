3bb8de06c98f8f93869dd5e65f0d434b7d56f727
Obuleswar <obuleswaror@gmail.com>
2025-09-30 00:22:01 +0000

    Try fixing this error: `Runtime Error: Error: (0 , {imported module [pro

--- src/components/UploadForm.tsx
+++ src/components/UploadForm.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { useActionState, useFormStatus } from 'react-dom';
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
 import { uploadAppAction, type State } from '@/app/upload/actions';
 import {
   Card,
